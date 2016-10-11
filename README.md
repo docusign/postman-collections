0f29e7cb9aa97e0cd5d71edf7a87318ca3c85e47
Nick Reed <nicholas.a.reed@gmail.com>
2016-10-11 15:28:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+### DocuSign and Postman 
+
+https://docusign.github.io/docusign-postman-api-collection
+
+Follow this guide to quickly and easily test the DocuSign API inside Postman. It includes examples for common workflows (obtain your accountID and baseUrl, sending an envelope via email, embedded signing, templates). 
+
+
+#### What is Postman? 
+
+https://www.getpostman.com/
+
+Postman is an application (Chrome extension or Mac app) that helps you develop your integration faster by making all of DocuSign's APIs available from a simple and developer-friendly interface. 
+
+
+#### License 
+
+The DocuSign Postman Collection is licensed under the MIT [License](LICENSE).
---
+++ public/app.js
@@ -0,0 +1,115 @@
+
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+$(function(){
+	
+	var lastEnv;
+
+	function newEnv(name){
+		var env = {
+			"id": generateUUID(),
+			"name": "DocuSign-" + name,
+			"values": [
+				{
+					"key": "hostenv",
+					"value": "",
+					"type": "text",
+					"enabled": true
+				},
+				{
+					"key": "apiVersion",
+					"value": "",
+					"type": "text",
+					"enabled": true
+				},
+				{
+					"key": "clientId",
+					"value": "",
+					"type": "text",
+					"enabled": true
+				},
+				{
+					"key": "username",
+					"value": "",
+					"type": "text",
+					"enabled": true
+				},
+				{
+					"key": "password",
+					"value": "",
+					"type": "text",
+					"enabled": true
+				}
+			],
+			"timestamp": 1468008305072,
+			"synced": false,
+			"syncedFilename": "",
+			"team": null,
+			"isDeleted": false
+		};
+		return env;
+	}
+
+	// $('#env').val(JSON.stringify(env,null,2));
+
+	$('#enter-creds').on('click', function(ev){
+		var options = {
+		    "name": "sampleBasicModal",
+		    "content": $("script#envModal").html(),
+		    onComplete: function(ev){
+		    	console.log(ev);
+		    	var modal = $(ev.currentTarget).closest('.modal');
+		    	var form = modal.find('form');
+		    	console.log(form);
+		    	
+		    	// console.log('s', form.serializeArray());
+		    	var formArr = form.serializeArray();
+
+		    	var envName = _.find(formArr,{name: 'hostenv'}).value.split('.')[0];
+		    	var envOutput = newEnv( envName );
+		    	var formObj = {};
+		    	// _.map(formArr, function(i){
+		    	// 	formObj[i.name] = i.value;
+		    	// 	// _.find(envOutput.values,{key: i.name}).value = i.value;
+		    	// });
+		    	formObj.hostenv = _.find(formArr,{name: 'hostenv'}).value;
+		    	formObj.apiVersion = _.find(formArr,{name: 'apiVersion'}).value;
+		    	formObj.clientId = _.find(formArr,{name: 'clientId'}).value;
+		    	formObj.username = _.find(formArr,{name: 'username'}).value;
+		    	formObj.password = _.find(formArr,{name: 'password'}).value;
+
+		    	console.log(formObj);
+		    	console.log(envOutput);
+
+		    	if(!_pm('env.create', 'DocuSign-' + envName, formObj)){
+		    		_pm('env.replace', 'DocuSign-' + envName, formObj); 
+		    	} 
+
+		    	lastEnv = envOutput;
+		    	// $('#envDownload')[0].click();
+		    	$('#hiddenEnv').removeClass('nodisplay');
+		    }
+		};
+		var inst = Olive.modal.show(options);
+
+		ev.preventDefault();
+	});
+
+	// var obj = {a: 123, b: "4 5 6"};
+	// var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+	// $('<a href="data:' + data + '" download="data.json">download JSON</a>').appendTo('#container');
+
+	$('#envDownload').on('click', function(){
+		var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(lastEnv));
+		$(this).attr('href','data:' + data);
+	});
+
+});
